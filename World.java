47d0b425f30c54ea142fedc88a3b79f49656d741
bbggr1209 <rsd3010@office.skhu.ac.kr>
2024-04-24 12:47:06 +0900

    fourth

--- World.java
+++ World.java
@@ -1,3 +1,2 @@
-public class World {
-    public static void run() { System.out.println("안녕하세요"); }
+public class World {public static void run() { System.out.println("안녕하세요"); System.out.println("김나윤"); }
 }
